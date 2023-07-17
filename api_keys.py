9fa56e785bfa8dc14634f908467fdefc23b0c592
VioletRogue12 <mmj649@gmail.com>
2023-07-17 20:10:03 +1000

    Mireille's EV Funding stuff

---
+++ api_keys.py
@@ -0,0 +1,8 @@
+# OpenWeatherMap API Key
+US_Census_key = "ddaeb0b8ea290503730d5e88987cf4f4829355a0"
+
+# Geoapify API Key
+geoapify_key = "46c5e86ef4e8435fa6e4dc1890a0fa9b"
+
+# OpenWeatherMap API Key
+weather_api_key = "cdd623533d451dea0445715a0119541b"
